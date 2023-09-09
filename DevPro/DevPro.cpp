cab02cf29e35d6b357611a77f793fa6a65096c20
Developer <Developer@DEVELOPER>
2023-09-10 01:08:21 +0530

    Add project files.

---
+++ DevPro/DevPro.cpp
@@ -0,0 +1,13 @@
+// DevPro.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include<iostream>
+using namespace std;
+
+// TODO: This is an example of a library function
+void fnDevPro()
+{
+	cout << "Hello How are you?";
+}
